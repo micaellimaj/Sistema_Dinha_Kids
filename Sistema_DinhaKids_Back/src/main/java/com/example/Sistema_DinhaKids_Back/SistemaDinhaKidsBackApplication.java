b7555e91e899c788e9cdb9f4163fd73dbc4de758
Julio Rodrigo <juliorodrigop19@gmail.com>
2024-04-16 09:44:10 -0300

    Adição Backend
    Foi adicionados os arquivos criados pelo spring initializr, NECESSARIO A CRIAÇÃO DE UM BANCO DE DADOS PARA FUNCIONAR O JPA!

---
+++ Sistema_DinhaKids_Back/src/main/java/com/example/Sistema_DinhaKids_Back/SistemaDinhaKidsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_DinhaKids_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDinhaKidsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDinhaKidsBackApplication.class, args);
+	}
+
+}
