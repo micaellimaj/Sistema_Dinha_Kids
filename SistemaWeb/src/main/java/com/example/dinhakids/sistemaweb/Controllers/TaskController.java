dcdeaab4c0f80ac11d33a7aa78d56879b7066601
Julio Rodrigo <juliorodrigop19@gmail.com>
2024-05-06 12:38:41 -0300

    adição do thymeleaf
    falta configurar o template resolver

--- SistemaWeb/src/main/java/com/example/dinhakids/sistemaweb/Controllers/TaskController.java
+++ SistemaWeb/src/main/java/com/example/dinhakids/sistemaweb/Controllers/TaskController.java
@@ -1,12 +1,9 @@
 package com.example.dinhakids.sistemaweb.Controllers;
 
-import com.example.dinhakids.sistemaweb.Models.UserModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import java.util.List;
 import java.util.ArrayList; // Importe a classe ArrayList
 import com.example.dinhakids.sistemaweb.Models.Task; // Importação da classe Task
@@ -18,7 +15,7 @@ public class TaskController {
 
     @GetMapping("/cadastrar")
     public String home() {
-        return "dashboard";
+        return "cadastrar";
     }
 
 
